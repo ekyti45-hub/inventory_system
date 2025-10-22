ca92254f49464a9524e36c2a339a0adda80c5e94
Ekayyy <ekyti45@gmail.com>
2025-10-22 12:53:37 +0700

    tambahkan file
    menambahkan satu file

---
+++ index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="id">
+<head>
+  <meta charset="UTF-8">
+  <title>Login Sistem Inventaris</title>
+  <link rel="stylesheet" href="style.css">
+</head>
+<body>
+  <div class="login-box">
+    <h2>Login Inventaris</h2>
+    <form action="auth/login.php" method="POST">
+      <input type="text" name="username" placeholder="Username" required><br>
+      <input type="password" name="password" placeholder="Password" required><br>
+      <button type="submit" name="login">Login</button>
+    </form>
+    <p>Belum punya akun? <a href="auth/register.php">Daftar di sini</a></p>
+  </div>
+</body>
+</html>
